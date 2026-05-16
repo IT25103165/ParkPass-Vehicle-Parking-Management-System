ec9d9083296708d4635172f7acf1e413132367f9
IT25102641 <IT25102641@my.sliit.lk>
2026-05-16 19:41:55 +0530

    BACKEND FILES FOR FEEDBACK & REPORT MANAGEMENT

---
+++ PublicFeedback.java
@@ -0,0 +1,5 @@
+package com.parking.system.entity;
+
+public class PublicFeedback extends Feedback {
+    // Basic feedback
+}
---
+++ VerifiedFeedback.java
@@ -0,0 +1,5 @@
+package com.parking.system.entity;
+
+public class VerifiedFeedback extends Feedback {
+    // Verified feedback (e.g. from users who actually parked)
+}
