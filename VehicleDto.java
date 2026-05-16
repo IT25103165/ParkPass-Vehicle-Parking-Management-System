734c7e64da7f0edb71baa4eb822c851112221cb4
it25101193 <it25101193@my.sliit.lk>
2026-05-16 18:28:51 +0530

    BACKEND CODE VEHICLE MANAGEMENT

---
+++ VehicleDto.java
@@ -0,0 +1,58 @@
+package com.parking.system.dto;
+
+public class VehicleDto {
+    private Long id;
+    private String plateNumber;
+    private String type;
+    private String brand;
+    private Long ownerId;
+    private String ownerName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPlateNumber() {
+        return plateNumber;
+    }
+
+    public void setPlateNumber(String plateNumber) {
+        this.plateNumber = plateNumber;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public Long getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(Long ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+}
